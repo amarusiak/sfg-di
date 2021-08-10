f8da2d0ee7d68564c2ff01471995a1c148d755c7
amarusiak <andrii.marusiak@gmail.com>
2021-08-10 11:53:03 +0300

    Added @Primary and @Profile

--- src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -1,16 +1,18 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 
 @Controller
-public class MyController {
+public class I18nController {
 
     private final GreetingService greetingService;
 
     //  NO Autowired
     //  NO @Qualifier
-    public MyController(GreetingService greetingService) {
+    public I18nController(@Qualifier("I18nService")GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
